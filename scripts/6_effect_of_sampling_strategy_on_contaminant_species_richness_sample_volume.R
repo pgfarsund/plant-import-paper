30d45cc065cad956938b6f6fca8cea393d8320b8
Peter Groth Farsund <pgfarsund@gmail.com>
2024-03-06 17:15:40 +0100

    getting there

--- scripts/6_effect_of_sampling_strategy_on_contaminant_species_richness_sample_volume.R
+++ scripts/6_effect_of_sampling_strategy_on_contaminant_species_richness_sample_volume.R
@@ -5,8 +5,9 @@
 # richness of contaminant plants and fungi identified by metabarcoding. we'll do
 # this in two steps:
 
-# we want to compare contaminant species detection between two sampling 
-# strategies for obtaining 20 samples per container: sampling 20 pots once, or
-# sampling 10 pots twice. 
-# we're interested in 1) the total number of species identified by both 
-# strategies, and 2) estimates of total richness based on the two strategies:
+# we want to compare contaminant species detection between two sample volumes. 
+# unfortunately, all our samples were approx. 25 grams each. we will therefore
+# simulate a double soil volume by merging samples that came from the same pots 
+# (i.e., 10 pots per container) PRIOR to normalizing reads, and compare the
+# observed species richness to that of pots only sampled once. 
+
